4600de4abca340ee5331078e52b1ae6f6426cdc1
Ravvsky <pawel.rawsk@gmail.com>
2023-08-17 00:44:12 +0200

    feat: add fonts to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {
       container: {
         center: true,
-        maxWidth: '144rem',
+        screens: {
+          '2xl': '1440px',
+        },
+      },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        second: ['Montserrat', ...defaultTheme.fontFamily.sans],
       },
     },
   },
