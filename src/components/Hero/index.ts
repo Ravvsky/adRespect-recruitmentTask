e7ccd225401447e61126d4af71eb5cafcb6ab284
Ravvsky <pawel.rawsk@gmail.com>
2023-08-18 23:42:23 +0200

    feat: add slider functions

---
+++ src/components/Hero/index.ts
@@ -0,0 +1,43 @@
+import '@splidejs/splide/css';
+import Splide from '@splidejs/splide';
+import appendComponentOnElement from '../../utils/appendComponentOnElement';
+const imageSlider = new Splide('#image-carousel', {
+  type: 'loop',
+  arrows: true,
+  pagination: false,
+});
+
+const contentSlider = new Splide('#left-carousel', {
+  type: 'fade',
+  arrows: false,
+  pagination: false,
+});
+
+imageSlider.sync(contentSlider);
+imageSlider.mount();
+contentSlider.mount();
+
+const buttonsWrapper = document.getElementById('buttonsWrapper');
+const childrenElements = buttonsWrapper && buttonsWrapper.querySelectorAll('*');
+childrenElements &&
+  childrenElements.forEach((element) => {
+    const componentName = (element as HTMLElement).getAttribute(
+      'data-componentName'
+    );
+
+    if (componentName) {
+      appendComponentOnElement(componentName);
+    }
+  });
+
+const heroComponent = document.querySelector(
+  '[data-componentName="Hero"]'
+) as HTMLElement;
+const imageSliderElement = document.getElementById(
+  'image-carousel'
+) as HTMLElement;
+const leftSliderWrapper = document.getElementById('leftSliderWrapper');
+
+const imageSliderHeight = getComputedStyle(imageSliderElement).height;
+heroComponent && (heroComponent.style.height = imageSliderHeight);
+leftSliderWrapper && (leftSliderWrapper.style.height = imageSliderHeight);
