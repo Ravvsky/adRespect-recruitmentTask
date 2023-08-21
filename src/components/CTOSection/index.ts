bf903fae231bf103a9caf91f128054dbcc741d52
Ravvsky <pawel.rawsk@gmail.com>
2023-08-21 22:14:14 +0200

    feat: add animation to CTOSection

--- src/components/CTOSection/index.ts
+++ src/components/CTOSection/index.ts
@@ -1,3 +1,28 @@
 import appendComponentOnElement from '../../utils/appendComponentOnElement';
-
+import { gsap } from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 appendComponentOnElement('Button');
+
+gsap.registerPlugin(ScrollTrigger);
+
+const ctoElement = document.querySelector('.cto-animate');
+
+if (ctoElement) {
+  gsap.set(ctoElement, { x: 0 });
+
+  const ctoAnimation = gsap.from(ctoElement, {
+    x: '100%',
+    duration: 1,
+    ease: 'power3.out',
+  });
+
+  const triggerPosition =
+    ctoElement.getBoundingClientRect().top + window.scrollY;
+
+  ScrollTrigger.create({
+    trigger: ctoElement,
+    animation: ctoAnimation,
+    start: triggerPosition + window.innerHeight * 0.8, // Adjust as needed
+    toggleActions: 'play none none reverse',
+  });
+}
